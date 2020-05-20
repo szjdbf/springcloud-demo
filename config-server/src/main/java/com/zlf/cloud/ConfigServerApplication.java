4d3e09a240be12d442faa510791916398b71cc59
zhanglifang <863348009@qq.com>
2020-05-20 17:32:46 +0800

    配置中心使用eureka

--- config-server/src/main/java/com/zlf/cloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/zlf/cloud/ConfigServerApplication.java
@@ -3,12 +3,14 @@ package com.zlf.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author zhanglifang
  * @date 2020/5/20
  */
 @SpringBootApplication
+@EnableEurekaClient
 @EnableConfigServer//开启分布式配置中心服务
 public class ConfigServerApplication {
 
