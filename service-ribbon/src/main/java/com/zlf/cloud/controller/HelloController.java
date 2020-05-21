59b2a8c1d4bce8c650dc9791aa89fcc39d0d1891
zhanglifang <863348009@qq.com>
2020-05-21 11:50:39 +0800

    eureka高可用

--- service-ribbon/src/main/java/com/zlf/cloud/controller/HelloController.java
+++ service-ribbon/src/main/java/com/zlf/cloud/controller/HelloController.java
@@ -1,9 +1,7 @@
 package com.zlf.cloud.controller;
 
-import brave.sampler.Sampler;
 import com.zlf.cloud.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
