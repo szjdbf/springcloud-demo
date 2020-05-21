42ea8d02158a445cc326a2b9d034c26c82b16f7e
zhanglifang <863348009@qq.com>
2020-05-21 15:27:51 +0800

    nacos作为注册中心

---
+++ service-gateway/src/main/java/com/zlf/cloud/ServiceGatewayApplication.java
@@ -0,0 +1,46 @@
+package com.zlf.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author zhanglifang
+ * @date 2020/5/21
+ */
+@SpringBootApplication
+@RestController
+public class ServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        String httpUri = "http://httpbin.org:80";
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri(httpUri))
+                .route(p -> p
+                        .host("*.hystrix.com")
+                        .filters(f -> f
+                                .hystrix(config -> config
+                                        .setName("mycmd")
+                                        .setFallbackUri("forward:/fallback")))
+                        .uri(httpUri))
+                .build();
+    }
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
+}
