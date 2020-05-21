09fea038740c22f71aae0467e12ac47ec68dc8c4
zhanglifang <863348009@qq.com>
2020-05-21 09:32:11 +0800

    增加消息总线bus配置

--- config-client/src/main/java/com/zlf/cloud/ConfigClientApplication.java
+++ config-client/src/main/java/com/zlf/cloud/ConfigClientApplication.java
@@ -1,10 +1,11 @@
 package com.zlf.cloud;
 
-import com.sun.org.apache.regexp.internal.RE;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/5/20
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 @RestController
 public class ConfigClientApplication {
 
