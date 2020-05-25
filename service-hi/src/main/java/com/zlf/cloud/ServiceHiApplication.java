5c216a5c85dd01f137906f6b8d504a353ed0d7c7
zhanglifang <863348009@qq.com>
2020-05-25 14:26:15 +0800

    使用高版本springboot和springcloud

--- service-hi/src/main/java/com/zlf/cloud/ServiceHiApplication.java
+++ service-hi/src/main/java/com/zlf/cloud/ServiceHiApplication.java
@@ -1,18 +1,15 @@
 package com.zlf.cloud;
 
-import brave.sampler.Sampler;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +27,7 @@ import java.util.Random;
 @EnableHystrix//开启断路器
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+@Slf4j
 public class ServiceHiApplication {
 
     public static void main(String[] args) {
@@ -45,8 +43,10 @@ public class ServiceHiApplication {
         Random random = new Random();
         //模拟异常
         if (3 == random.nextInt(5)) {
+            log.error(this.getClass().getName() + " --> 出现异常");
             throw new RuntimeException();
         }
+        log.info("正常");
         return "hi " + name + ", i am from port : " + port;
     }
 
@@ -55,19 +55,4 @@ public class ServiceHiApplication {
         return "hi," + name + ",sorry,error!";
     }
 
-    @Bean
-    public Sampler defaultSampler() {
-        return Sampler.ALWAYS_SAMPLE;
-    }
-
-
-    @Bean
-    public ServletRegistrationBean getServlet() {
-        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
-        registrationBean.setLoadOnStartup(1);
-        registrationBean.addUrlMappings("/actuator/hystrix.stream");
-        registrationBean.setName("HystrixMetricsStreamServlet");
-        return registrationBean;
-    }
 }
